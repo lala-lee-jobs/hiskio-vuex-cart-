9b5e0de92e495d0e5dd724796296ba7ede02bcff
lala.lee <lala.lee@mitake.com.tw>
2020-03-31 14:49:28 +0800

    拆分出LessonItem組件

---
+++ src/LessonItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img :src="lesson.image" width="100"/>
+    <span class="title">{{lesson.title}}</span>
+    <span class="price">{{lesson.price}}</span>
+    <span class="lecturers">{{lecturers}}</span>
+  </div>
+</template>
+<script>
+export default {
+  props: ['lesson'],
+  computed: {
+    lecturers () {
+      return this.lesson.lecturers
+        .map(lecturer => lecturer.username)
+        .join(',')
+    }
+  }
+}
+</script>
--- src/LessonList.vue
+++ src/LessonList.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <h2>LessonList</h2>
-    <div v-for="(lesson, index) in lessons" :key="index">
-      <span>{{lesson.title}}</span>
-    </div>
+    <LessonItem
+      v-for="(lesson, index) in lessons"
+      :lesson="lesson"
+      :key="index">
+    </LessonItem>
   </div>
 </template>
 <script>
 import { mapState, mapActions } from 'vuex'
+import LessonItem from './LessonItem'
 
 export default {
+  components: {
+    LessonItem
+  },
   computed: {
     ...mapState(['lessons'])
   },
